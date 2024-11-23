7a09f1447d5152d2f9e2202fb14410f6def74aa9
Logan Wilkerson <lcwilker@gmail.com>
2024-11-22 20:52:48 -0500

    idk

--- ui/life_bar.gd
+++ ui/life_bar.gd
@@ -5,7 +5,7 @@ func initialize(player: Player):
 	player.on_health_changed.connect(_handle_player_health_changed)
 	var remaining = float(player.current_health) / float(player.max_health)
 	material.set_shader_parameter('remaining', remaining);
-	material.set_shader_parameter('units', 5);	
+	material.set_shader_parameter('units', 5);
 
 
 func _handle_player_health_changed(new_health: int, max_health: int) -> void:
