13a65ce51f4e74958021e679256673683b0c49e2
Logan Wilkerson <lcwilker@gmail.com>
2024-11-23 00:57:17 -0500

    Resource bar script. Added in shield and health bar.

--- world/world.gd
+++ world/world.gd
@@ -5,6 +5,8 @@ extends Node2D
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	$HUD/LifeBar.initialize(player_ship)
+	$HUD/HealthBar.initialize(player_ship.on_health_changed, player_ship.current_health, player_ship.max_health)
+	$HUD/ShieldBar.initialize(player_ship.on_shield_changed, player_ship.current_shield, player_ship.max_shield)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
